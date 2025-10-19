47c02953c0fc1aaceedcd05418b7335577a14205
Andrew Zunt <andrew.zunt@gmail.com>
2025-10-19 01:43:45 -0700

    Adding APIG to trigger lambda, working! (mostly)

--- lib/shared-resources-stack.ts
+++ lib/shared-resources-stack.ts
@@ -2,8 +2,12 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { join } from "path";
+import { aws_apigatewayv2 } from 'aws-cdk-lib';
 import { aws_lambda } from 'aws-cdk-lib';
 import { aws_lambda_nodejs } from 'aws-cdk-lib';
+import { aws_logs } from 'aws-cdk-lib';
+import { Duration } from 'aws-cdk-lib';
+import { HttpLambdaIntegration } from 'aws-cdk-lib/aws-apigatewayv2-integrations';
 
 export class SharedResourcesStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -16,10 +20,22 @@ export class SharedResourcesStack extends cdk.Stack {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
 
-    const function = new aws-lambda-nodejs.NodejsFunction(this, "SharedResourcesHome", {
-      entry: join(__dirname, '../index.js'),
+    const lambda = new aws_lambda_nodejs.NodejsFunction(this, 'SharedResourcesHome', {
+      entry: join(__dirname, './lambda/handler.js'),
       runtime: aws_lambda.Runtime.NODEJS_20_X,
-      logRetention: RetentionDays.ONE_WEEK,
+      handler: 'main',
+      logRetention: aws_logs.RetentionDays.ONE_WEEK,
+      timeout: Duration.minutes(1),
+    });
+
+    const api = new aws_apigatewayv2.HttpApi(this, 'SharedResourcesAPI', {
+      disableExecuteApiEndpoint: true, // set to false to re-enable
+    });
+    const lambdaIntegration = new HttpLambdaIntegration('SharedResourcesIntegration', lambda);
+    api.addRoutes({
+      path: '/',
+      methods: [ aws_apigatewayv2.HttpMethod.ANY ],
+      integration: lambdaIntegration,
     });
   }
 }
